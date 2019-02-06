91eadeaeba02a7d9558b057817870a61cb34e209
Kalli <kallirroi.retzepi@gmail.com>
2019-02-05 20:57:17 -0500

    pyaudio code (couldn't git commit from raspi)

--- button_LED.py
+++ button_LED.py
@@ -7,30 +7,27 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setup(7, GPIO.OUT)  # LED == 7
 GPIO.output(7, False)  # Set output to off
-GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Button == 11
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button == 11
 playback_index = 0
 
 def updateFileList():
     print("updating file list")
-    return [ f for f in listdir('./audio/playback/') if f[-4:] == '.mp3' ]
+    return [ f for f in listdir('./audio/playback/') if f[-4:] == '.mp3' ] # read files from the /audio/playback folder
 mp3_files = updateFileList()
 if not (len(mp3_files) > 0):
     print("No mp3 files found!")
 
 def buttonCallback(channel):
-    if (GPIO.input(7) == False):
+    if (GPIO.input(7) == False): # if the LED is not on, start recording
         print("started recording!")
-        # os.system('arecord -D plughw:1 --duration=10 -c 2 -f cd  ./audio/playback/testLED.mp3')
-        os.system('arecord -D plughw:1 -c 2 -f cd ./audio/playback/testLED.mp3')
         GPIO.output(7, True) #LED on
-        #kill omxplayer
+        os.system('arecord -D plughw:1 -c 2 -f cd ./audio/playback/test.mp3')
     else:
-        print("stopped recording!")
+        print("stopped recording!") # if the LED is on, then we were recording and have to stop.
         GPIO.output(7, False)
-
     mp3_files = updateFileList()
 
-GPIO.add_event_detect(11, GPIO.RISING, callback=buttonCallback, bouncetime=100)
+GPIO.add_event_detect(11, GPIO.FALLING, callback=buttonCallback, bouncetime=100)
 
 while True:
     # loop audio
@@ -38,9 +35,14 @@ while True:
     if playback_index >= len(mp3_files):
         playback_index = 0
 
-    if not GPIO.input(11): #if button hasn't been pressed
-        print("currently playing: "+mp3_files[playback_index])
+    if GPIO.input(7) == False: #if not recording, you should be playing!
+        print("playing: "+mp3_files[playback_index])
         os.system('omxplayer -o local ./audio/playback/'+mp3_files[playback_index] + ' &')
         sleep(1)
+    else:
+        print("recording")
+        sleep(1)
+
+    sleep(1)
 
 GPIO.cleanup()
