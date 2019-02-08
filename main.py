5602b5adaa8e8aeb6e4d80761ff406f50c3987a7
kallirroi <kallirroi.retzepi@gmail.com>
2019-02-08 08:46:46 -0500

    segmentation fault when button is pressed twice

--- main.py
+++ main.py
@@ -13,49 +13,53 @@ gpio.setup(17, gpio.IN, pull_up_down=gpio.PUD_UP)
 
 class ButtonRecorderPlayer(object):
     def __init__(self):
-        # gpio.setup(23, gpio.IN, pull_up_down=gpio.PUD_UP)
         self.rec = Recorder(channels=1)
         self.play = Player()
         self.p = pyaudio
 
-    def start(self):
-        input("Press Enter to start recording...")
-        self.start_recording()
-        # gpio.add_event_detect(23, gpio.FALLING, callback=self.falling, bouncetime=10)
+    # def start(self):
+    #     input("Press Enter to start recording...")
+    #     self.start_recording()
+        # gpio.add_event_detect(17, gpio.FALLING, callback=self.falling, bouncetime=10)
 
     def start_recording(self, channel=1):
-        # gpio.remove_event_detect(23)
+        gpio.remove_event_detect(17)
         print ('Recording')
-        # gpio.add_event_detect(23, gpio.RISING, callback=self.rising, bouncetime=10)
         gpio.output(4, True) #LED on
         timestr = time.strftime("%Y%m%d-%H%M%S")
-        self.recfile = self.rec.open('recordings/' + timestr + '.wav', self.p, 'wb')
-        self.recfile.start_recording()
+        # self.recfile = self.rec.open('recordings/' + timestr + '.wav', self.p, 'wb')
+        # self.recfile.start_recording()
 
-        input("Press Enter to stop recording...")
-        self.stop_recording()
+        time.sleep(0.5)
+        gpio.add_event_detect(17, gpio.FALLING, callback=self.stop_recording, bouncetime=100)
+        # input("Press Enter to stop recording...")
+        # self.stop_recording()
 
-    def stop_recording(self, channel=1):
-        # gpio.remove_event_detect(23)
-        print ('Recording Stopped')
-        # gpio.add_event_detect(23, gpio.FALLING, callback=self.falling, bouncetime=10)
+    def stop_recording(self, channel=1): # this should automatically break to "start playback"
+        gpio.remove_event_detect(17)
+        print ('Stopped recording')
         gpio.output(4, False) #LED on
-        self.recfile.stop_recording()
-        self.recfile.close()
 
-        # self.start_playback()
+        # self.recfile.stop_recording()
+        # self.recfile.close()
+        gpio.add_event_detect(17, gpio.FALLING, callback=self.start_recording, bouncetime=100)
+        self.start_playback()
 
     def start_playback(self, channel=1):
         print ('playback starting')
-        self.play.play('recordings', self.p)
-        input("Press Enter to start recording...")
-        self.start_recording()
+        # self.play.play('recordings', self.p)
 
+        # input("Press Enter to start recording...")
+        # self.start_recording()
+        time.sleep(0.5)
 
 recPlayBtn = ButtonRecorderPlayer()
-recPlayBtn.start()
+# recPlayBtn.start()
+gpio.add_event_detect(17, gpio.FALLING, callback=recPlayBtn.start_recording, bouncetime=100)
 
 while True:
+
     recPlayBtn.start_playback()
+    time.sleep(1)
 
 gpio.cleanup()
--- old/button_LED.py
+++ old/button_LED.py
@@ -10,34 +10,34 @@ GPIO.output(7, False)  # Set output to off
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button == 11
 playback_index = 0
 
-def updateFileList():
-    print("updating file list")
-    return [ f for f in listdir('./audio/playback/') if f[-4:] == '.mp3' ] # read files from the /audio/playback folder
-mp3_files = updateFileList()
-if not (len(mp3_files) > 0):
-    print("No mp3 files found!")
+# def updateFileList():
+#     print("updating file list")
+#     return [ f for f in listdir('./recordings' if f[-4:] == '.wav' ] # read files from the /audio/playback folder
+# mp3_files = updateFileList()
+# if not (len(mp3_files) > 0):
+#     print("No mp3 files found!")
 
 def buttonCallback(channel):
     if (GPIO.input(7) == False): # if the LED is not on, start recording
         print("started recording!")
         GPIO.output(7, True) #LED on
-        os.system('arecord -D plughw:1 -c 2 -f cd ./audio/playback/test.mp3')
+        # os.system('arecord -D plughw:1 -c 2 -f cd ./audio/playback/test.mp3')
     else:
         print("stopped recording!") # if the LED is on, then we were recording and have to stop.
         GPIO.output(7, False)
-    mp3_files = updateFileList()
+    # mp3_files = updateFileList()
 
 GPIO.add_event_detect(11, GPIO.FALLING, callback=buttonCallback, bouncetime=100)
 
 while True:
     # loop audio
     playback_index += 1
-    if playback_index >= len(mp3_files):
-        playback_index = 0
+    # if playback_index >= len(mp3_files):
+    #     playback_index = 0
 
     if GPIO.input(7) == False: #if not recording, you should be playing!
-        print("playing: "+mp3_files[playback_index])
-        os.system('omxplayer -o local ./audio/playback/'+mp3_files[playback_index] + ' &')
+        print("playing: ")
+        # os.system('omxplayer -o local ./audio/playback/'+mp3_files[playback_index] + ' &')
         sleep(1)
     else:
         print("recording")
