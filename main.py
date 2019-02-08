3fefef59c2eacd212425924e9edf5e7be2ee4e5a
Kallirroi <kallirroi.retzepi@gmail.com>
2019-02-08 17:20:23 -0500

    I think the state machine works ok...?

--- main.py
+++ main.py
@@ -20,47 +20,44 @@ class ButtonRecorderPlayer(object):
         print ('Recording')
         gpio.output(4, True) #LED on
         timestr = time.strftime("%Y%m%d-%H%M%S")
-        self.recfile = self.rec.open('recordings/' + timestr + '.wav', self.p, 'wb')
+        # self.recfile = self.rec.open('recordings/' + timestr + '.wav', self.p, 'wb')
         # self.recfile.start_recording() # non blocking mode
-        self.recfile.record(duration = 10.0) # blocking mode
-        time.sleep(0.5)
+        # self.recfile.record(duration = 10.0) # blocking mode
+        time.sleep(0.25)
 
 
     def stop_recording(self, channel=1): # this should automatically break to "start playback"
         print ('Stopped recording')
         gpio.output(4, False) # LED is OFF when playing
-        self.recfile.stop_recording()
-        self.recfile.close()
-        time.sleep(0.5)
+        # self.recfile.stop_recording()
+        # self.recfile.close()
+        time.sleep(0.25)
 
     def start_playback(self, channel=1):
         gpio.output(4, False) # LED is OFF when playing
         print ('Playing')
-        self.play.play('recordings', self.p)
-        time.sleep(0.5)
+        # self.play.play('recordings', self.p)
+        time.sleep(0.25)
 
 recPlayBtn = ButtonRecorderPlayer()
 state = False
 def getState(state):
-    if (gpio.input(17)==1):
-        print ('button!')
+    if (gpio.input(17) == 1):
         state = not state
     return state
-
 
 while True:
     prevState = state
     state = getState(prevState)
 
-    if ( state == False ): # playing
-        print('playing')
-        # recPlayBtn.start_playback()
-    elif ( prevState == False and state == True):
-        # recPlayBtn.start_recording()
-        print ('started recording') # recording
-    elif (prevState == True and state == False):
-        # recPlayBtn.stop_recording()
-        print ('stopped recording') # stopped recording
+    if (state == False):
+        if (prevState == True): # state is False and prevState is True => stop recording
+            recPlayBtn.stop_recording()
+        else: # state is False and prevState is False => play
+            recPlayBtn.start_playback()
+    else:
+        if (prevState == False): # state is True and prevState is False => start recording
+            recPlayBtn.start_recording()
 
     # Now wait a while
     time.sleep(0.25)
