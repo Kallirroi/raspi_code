7cbc673c563d000dfb80d1876f6aaacf242e5a71
Kalli <kallirroi.retzepi@gmail.com>
2019-02-11 15:55:03 -0500

    Update receive.js

--- receive.js
+++ receive.js
@@ -3,7 +3,7 @@ var Dat = require('dat-node')
 // 1. Tell Dat where to download the files
 Dat('./', {
   // 2. Tell Dat what link I want
-  key: '' // (a 64 character hash from above)
+  key: 'e6ce719856e903693d4211a1f7d96d89753b639b4a3fa564de6f2e0009717963' // (a 64 character hash from above)
 }, function (err, dat) {
   if (err) throw err
 
