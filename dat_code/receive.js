51fc397ab160a44fccfce09f5a3c8c54a57cb841
Kalli <kallirroi.retzepi@gmail.com>
2019-02-21 18:58:42 -0500

    Update receive.js

--- dat_code/receive.js
+++ dat_code/receive.js
@@ -3,7 +3,7 @@ var Dat = require('dat-node')
 // 1. Tell Dat where to download the files
 Dat('./recordings/', {
   // 2. Tell Dat what link I want to sync with
-  key: 'f1877ad69194f200226ac9f66c679acd0e11c4278a48b69fb6042680767c5ee0' // (a 64 character hash from above)
+  key: '' // (a 64 character hash from above)
 }, function (err, dat) {
   if (err) throw err
 
